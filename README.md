862e912c8c5eb8863e935495aa2601f2db4ad7d0
Martin Schrimpf <m4rtinsch@gmail.com>
2022-06-25 10:03:14 -0400

    prepare Omnivore Brain-Score submission

---
+++ README.md
@@ -0,0 +1,7 @@
+# Submission of Omnivore models to Brain-Score
+
+Submit [Omnivore models](https://github.com/facebookresearch/omnivore)
+by [Girdhar*, Singh*, Ravi*, et al. 2022](https://arxiv.org/abs/2201.08377)
+to [Brain-Score](http://www.brain-score.org/).
+
+Following the [submission template](https://github.com/brain-score/sample-model-submission).
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requirements = [
+    "model-tools @ git+https://github.com/brain-score/model-tools",
+    "pytorch",
+    "torchvision",
+    "torchaudio",
+    "einops",
+    "pytorchvideo",
+    "timm",
+]
+
+setup(
+    name='model-template',
+    version='0.1.0',
+    description="An example project for adding brain or base model implementation",
+    author="Franziska Geiger",
+    author_email='fgeiger@mit.edu',
+    url='https://github.com/brain-score/brainscore_model_template',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='brain-score template',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+)
